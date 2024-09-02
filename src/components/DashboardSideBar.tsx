80b08b7813b5dd65515a06351939d6bcdaee09b9
Mohamed Ibrahim <Mohamedibrahim208209@gmail.com>
2024-09-02 15:22:28 +0300

    modified:   src/components/DashboardSideBar.tsx

--- src/components/DashboardSideBar.tsx
+++ src/components/DashboardSideBar.tsx
@@ -2,15 +2,35 @@
 
 import { DashboardItems } from "./DashboardItems";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export function DashboardSideBar() {
   const [isOpen, setIsOpen] = useState(false);
+  const sidebarRef = useRef<HTMLDivElement>(null);
   // TODO: get admin from database
   const admin = "mariam-ali";
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        sidebarRef.current &&
+        !sidebarRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [sidebarRef]);
+
   return (
-    <aside className="md:border md:border-gray-200 md:rounded-3xl md:flex md:flex-col md:gap-8 md:h-[90vh] md:my-[5vh] md:py-6 md:sticky md:top-0">
+    <aside
+      ref={sidebarRef}
+      className="md:border md:border-gray-200 md:rounded-3xl md:flex md:flex-col md:gap-8 md:h-[90vh] md:my-[5vh] md:py-6 md:sticky md:top-0"
+    >
       <div className="flex items-center justify-between p-4 md:p-0 md:mx-auto md:justify-center sticky top-0 bg-white z-10">
         <button
           className="md:hidden"
