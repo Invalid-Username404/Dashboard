1f79fc5fc6556cd990ec4b4d9bff3399148bd282
Invalid-Username404 <mohamedibrahim208209@gmail.com>
2024-09-12 00:00:19 +0300

    modified:   src/app/login/page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,6 +9,13 @@ export const metadata = {
 export default function Login() {
   return (
     <div className="bg-primary flex flex-col items-center justify-evenly h-screen p-4 sm:p-8">
+      <p className="text-orange-600  text-2xl font-bold">
+        Pssst... You can use the following credentials:
+        <br />
+        <span className="text-red-600  text-xl font-bold">
+          user: test@task.com <br /> password: 123456
+        </span>
+      </p>
       <Image
         src="/cyparta-logo.svg"
         alt="Cyparta Logo"
